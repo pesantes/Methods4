d984ecf12ac94277353a6aebf56b3fbf05c63e56
Fernando Pesantes <pesantes@usf.edu>
2019-01-31 20:18:46 -0500

    Version 2 to upload to github for turn in.

--- Methods4/Program.cs
+++ Methods4/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+Author: Fernando Pesantes
+Date: 1/31/2019
+Description: Methods class project
+
+*/
+
+using System;
 
 namespace Methods4
 {
@@ -6,7 +13,28 @@ namespace Methods4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your full name");
+            Console.ReadKey(true);
+            try
+            {
+                string input = Console.ReadLine();
+                DisplayName(input);
+            } // End of try
+            catch
+            {
+                Console.WriteLine("Please enter your name next time");
+            }// End of catch
         }
-    }
-}
+
+        // Method to display user input with a greeting. 
+        private static void DisplayName(string input)
+        {
+            string FullName = input;
+            Console.WriteLine("");
+            Console.WriteLine("Hello " + input);
+            Console.ReadKey(true);
+        }// End of method
+
+    } // end of class
+
+} // End of namespace
